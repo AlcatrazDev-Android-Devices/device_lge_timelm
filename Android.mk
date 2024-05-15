45deb4618c5dac1df0ec23713cfc56ddec3a5f59
Alcatraz323 <alcatraz32323@gmail.com>
2024-05-15 12:06:30 +0800

    timelm: create symlink for camera lib libc++_shared
    Change-Id: If6c1ca53e6e953cdb49b0972f93711df2f843a5f

--- Android.mk
+++ Android.mk
@@ -8,4 +8,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter timelm,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+CAMERA_CPP_SHARED_SYMLINK := $(TARGET_OUT_VENDOR)
+$(CAMERA_CPP_SHARED_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating camera c++ shared libs symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf /vendor/lib/libc++_shared_snpe.so $@/lib/libc++_shared.so
+	$(hide) ln -sf /vendor/lib64/libc++_shared_snpe.so $@/lib64/libc++_shared.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CAMERA_CPP_SHARED_SYMLINK)
+
 endif
