11a7484ca92c3f6eb52af3fb981af7a406f28e7f
Mohamed Moussa <mmouss@codeaurora.org>
2024-05-31 01:08:37 +0800

    timelm: rootdir: Move init.recovery.qcom.rc out of root
    The file init.recovery.qcom.rc flagged as offending file in root/,
    because it violates the pathing req. (system/ & root/). Place the file
    in $(TARGET_RECOVERY_ROOT_OUT)
    
    Change-Id: I48ec1586756f83073bd8264d80ecb7226de44d94

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,5 +25,5 @@ LOCAL_MODULE       := init.recovery.timelm.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.timelm.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
